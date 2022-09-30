ffd62516a132fe92f8b049df4bc546146f7dbe39
Ronald <ronald.isael@hotmail.com>
2022-09-29 22:00:59 -0400

    Disenio Minemalista hecho

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
-export default function App (){
-  return(
+
+export default function App() {
+  return (
     <div>
-      <h1>Hello World!</h1>
+      <h1 className="text-center">Login</h1>
+      <div className="form-group text-center">
+        <label>Usuario: </label>
+
+        <br />
+        <input type="text" className="form-control" name="username" />
+
+        <br />
+        <label>Contraseña: </label>
+        <br />
+        <input type="password" className="form-control" name="password" />
+
+        <br/>
+        <button className="btn btn-primary" >Iniciar Sesion</button>
+        
+      </div>
     </div>
   );
 }
