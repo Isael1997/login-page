0cc7c4c4883e8be7361abb2e917a59a574315c6e
Ronald <ronald.isael@hotmail.com>
2022-10-07 19:22:18 -0400

    Se arreglo el conflito con la rama de design

---
+++ src/Login.js
@@ -0,0 +1,32 @@
+
+import React from "react"
+import "./Login.css"
+
+
+export default function Login() {
+	return (
+		<div className="login-box">
+			<h2>Login</h2>
+			<form>
+
+				<div className="user-box">
+					<input type="text" name="" required="" />
+					<label>Username</label>
+				</div>
+
+				<div className="user-box">
+					<input type="password" name="" required="" />
+					<label>Password</label>
+				</div>
+
+				<div className="button-form">
+					<a id="submit" href="#">submit</a>
+				</div>
+				<div id="register">
+					Don't have an acount?
+					<a href="#"> Register</a>
+				</div>
+			</form>
+		</div>
+	)
+}
