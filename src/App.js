98c8bb3eaba8d01912eaef2973dad57afa2dfd44
Ronald <ronald.isael@hotmail.com>
2022-10-07 19:22:18 -0400

    El login tiene un buen disenio, falta el registro

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
 import React from "react";
 import "./App.css";
-
+import Login from "./Login.js";
 
 export default function App() {
   return (
-    <div>
-
-      <section className="vh-50 gradient-custom">
-        <div className="container py-5 h-50">
-          <div className="row d-flex justify-content-center align-items-center h-50">
-            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-              <div className="card bg-dark text-white">
-                <div className="card-body p-5 text-center">
-
-                  <div className="mb-md-5 mt-md-4 pb-5">
-                    <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-                    <p className="text-white-50 mb-5">Please enter your login and password!</p>
-
-                    <div className="form-outline form-white mb-4">
-                      <input type="text" className="form-control form-control-lg" placeholder="User"/>
-                    </div>
-
-                    <div className="form-outline form-white mb-4">
-                      <input type="password" className="form-control form-control-lg" placeholder="Pasword"/>
-                    </div>
-
-                    <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
-
-                    <button className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-    </div>
+    <Login/>
   );
 }
